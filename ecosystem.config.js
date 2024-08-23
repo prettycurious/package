e91c019153c15424e30f80fae4196308be7004d5
田文渊 <tianwenyuan@suitbim.com>
2024-08-23 10:13:19 +0800

    完善pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,25 @@ module.exports = {
     name: "unpkg",
     script: "start.js",
     cwd: '/unpkg/',
+    env: {
+      NODE_ENV: "development",
+      NPM_REGISTRY_URL: "https://registry.npmmirror.com"
+    },
     env_production: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      NPM_REGISTRY_URL: "https://registry.npmmirror.com"
     }
   },{
     name: "verdaccio",
     script: "verdaccio",
-    args: "--listen 0.0.0.0:4873"
+    args: "--listen 0.0.0.0:4873",
+    env: {
+      NODE_ENV: "development",
+      NPM_REGISTRY_URL: "https://registry.npmmirror.com"
+    },
+    env_production: {
+      NODE_ENV: "production",
+      NPM_REGISTRY_URL: "https://registry.npmmirror.com"
+    }
   }]
 };
